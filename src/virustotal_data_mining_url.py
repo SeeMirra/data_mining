673be789495d9e79bdb9b7c7de884a7a4fae5430
gitaziabari <gita.ziabari@fidelissecurity.com>
2016-05-12 12:02:07 -0700

    Update virustotal_data_mining_url.py

--- src/virustotal_data_mining_url.py
+++ src/virustotal_data_mining_url.py
@@ -14,15 +14,15 @@ url_report = analyzer.process_package(package_path)
 for param in url_report:
     url_positives = param.get("positives")
     url = param.get("url")
-    if url_positives > 8:
+    if url_positives > 6:
            data_report = {}
            info = param.get("additional_info")
            url_threat_score = analyzer.get_url_threat_score(info)
            engines_lst = param.get("scans")
            engines = analyzer.get_detected_engine_list(engines_lst)
            engine_score = analyzer.get_engine_score(engines)
-           if engine_score > 10:
-              if url_threat_score >8:
+           if engine_score > 8:
+              if url_threat_score > 6:
                  short_url = analyzer.get_short_url(url)
                  url_lst = analyzer.get_url_from_data_file(url_data)
                  if short_url in url_lst:
