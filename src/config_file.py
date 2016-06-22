6718f682c6881facf4a90047c92933eeca496d89
gitaziabari <gita.ziabari@fidelissecurity.com>
2016-06-21 22:53:54 -0400

    Update config_file.py

--- src/config_file.py
+++ src/config_file.py
@@ -12,7 +12,7 @@ high_scored_hashes = "/tmp/high_scored_hashes.csv"
 url_data = "/tmp/url_data.csv"
 
 #place the directory of bin folder here
-bin_dir = "/work/data_mining_isoi/bin"
+#bin_dir = <"/data_mining-master/bin">
 
 #specify the criteria that you would like to use in VTI search
 search_lst = [{"type": "executable", "positives": "7"+"+", "size":"90kb+"}, 
